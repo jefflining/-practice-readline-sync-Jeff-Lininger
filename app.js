5c03b13594f8d506015429eac25c8f2364489807
jeff <jefflining@gmail.com>
2025-03-03 11:36:31 -0600

    final commit

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const readline = require('readline-sync')
 
+const input = require('readline-sync');
+
 let name = readline.question("What is your name? ");
 
 console.log("Hello, " + name + "!")
 
-test
+let value = readline.question("What is the name for a piece of data that JavaScript can store, process and interact with? ");
+
+console.log("You entered, " + value + ".")
+
+let primitive = readline.question("What is the most basic type of data called in JavaScipt? ");
+
+console.log("You entered, " + primitive + ".")
+
+let string = readline.question("In JavaScript what is name of a value that consists of test wrapped in double quotes? ");
+
+console.log("You entered, " + string + ".")
+
+let number = readline.question("What data type is used to represent and work with a numerical value in JavaScript? ");
+
+console.log("You entered, " + number + ".")
+
+let numExample = input.questionInt("Enter a number: ");
+
+console.log("You entered, " + numExample + ".")
+
+let num1 = input.questionInt("Enter a number: ");
+
+
+
